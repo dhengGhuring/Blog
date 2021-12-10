b2de9085a4a1a3889507fccf738e33851ae3cb35
dhengGhuring <wyvrendragon@gmail.com>
2021-12-11 01:26:31 +0700

    add new slick slider for articles content

---
+++ script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+//     $('.warp').slick({
+//         autoplay: false,
+//     })
+// })
+// $(document).ready(function () {
+//     $('.warp').slick({
+        
+//     });
+// });
+$('.warp').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });

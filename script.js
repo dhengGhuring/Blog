f45a8ed782ed833d04e7666df67e5141414f3d16
dhengGhuring <wyvrendragon@gmail.com>
2021-12-14 10:46:46 +0700

    tugas3 done

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ $('.warp').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    autoplay: false,
-    autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2500,
   });
